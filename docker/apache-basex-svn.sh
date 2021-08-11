aa7471e269d8a76c6a87c8e3ec439592f542c25c
Gerrit Imsieke <gerrit.imsieke@le-tex.de>
2021-08-11 10:04:58 +0200

    document why we passed the PATH env var to sudo like this

--- docker/apache-basex-svn.sh
+++ docker/apache-basex-svn.sh
@@ -4,6 +4,9 @@ echo $1 _ $2 _ $3 _ $4
 
 sudo -u basex echo $(whoami) $JAVA_HOME _ $PATH
 
+# https://unix.stackexchange.com/questions/83191/how-to-make-sudo-preserve-path#answer-83194
+# (remember we are on Debian because the Alpine apache/svn combo gave us segfaults when using authz)
+
 sudo -u basex env "PATH=$PATH" java -version
 
 sudo -u basex env "PATH=$PATH" basex/bin/basexhttp -h$1 -p$2 -s$3 -S
