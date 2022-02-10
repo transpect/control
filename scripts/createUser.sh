b0c578e02b546dc9a2b40d7049b6c04380ea2de3
Paul Markwart <paul.markwart@outlook.de>
2022-02-10 12:28:04 +0100

    Common Name in cert is username

--- scripts/createUser.sh
+++ scripts/createUser.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-usage() { echo "Usage: $0 -u username -a path/to/ca-certs [-p password] [-d days]" 1>&2; exit 1; }
+usage() { echo "${error} Usage: $0 -u username -a path/to/ca-certs [-p password] [-d days]" 1>&2; exit 1; }
+
 
 while getopts ":u:a:" o; do
     case "${o}" in
 	u)
 	    username=${OPTARG}
-	    [ -z "$username" ] || usage
 	    ;;
 	a)	    
 	    capath=${OPTARG}
-	    [ -z "$capath" ] || usage
 	    ;;
 	p)
-	    password=${OPTARG}
-	    [ -z "$password" ] || password="xxxx"
+	    password=${OPTARG}    
 	    ;;
 	d)
 	    days=${OPTARG}
-	    [ -z "$days" ] || days="365"
 	    ;;
 	*)
 	    usage
@@ -31,6 +28,9 @@ if [ -z "${username}" ] || [ -z "${capath}" ]; then
     usage
 fi
 
+[ ! -z "${password}" ] || password="xxxx"
+[ ! -z "${days}" ] || days="365"
+
 echo ${username}
 echo ${capath}
 echo ${password}
@@ -38,15 +38,17 @@ echo ${days}
 
 openssl genrsa -aes256 -passout pass:${password} -out ${username}.pass.key 4096
 openssl rsa -passin pass:${password} -in ${username}.pass.key -out ${username}.key
+
 rm ${username}.pass.key
 
-openssl req -new -key ${username}.key -out ${username}.csr
+echo "${username}.key created"
 
-openssl x509 -req -days ${days} -in ${username}.cst -CA ${capath}/ca.pem -CAkey ${capath}/ca.key -out ${username}.pem
+openssl req -new -key ${username}.key -out ${username}.csr -subj "/C=DE/ST=Saxony/L=Leipzig/O=LeTex/CN=${username}"
+openssl x509 -req -days ${days} -in ${username}.csr -CA ${capath}/ca.pem -CAkey ${capath}/ca.key -CAcreateserial -out ${username}.pem
 echo "${username}.pem created"
 
-cat ${username}.key ${username}.pem ca.pem > ${username}.full.pem
+cat ${username}.key ${username}.pem ${capath}/ca.pem > ${username}.full.pem
 echo "${username}.full.pem created"
 
-openssl pkcs12 -export -out ${username}.full.pfx -inkey ${username}.key -in ${username}.pem -certfile ${capath}/ca.pem
+openssl pkcs12 -export -passout pass:${password} -out ${username}.full.pfx -inkey ${username}.key -in ${username}.pem -certfile ${capath}/ca.pem
 echo "${username}.full.pfx created"
